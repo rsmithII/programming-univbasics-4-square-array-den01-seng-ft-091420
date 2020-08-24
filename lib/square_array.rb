1fd62a18485d7c54e7d5e8f9ce0e41168b441320
Rodney Smith II <r_smith3@hotmail.com>
2020-08-24 19:16:43 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,3 +1,12 @@
 def square_array(array)
-  # your code here
+  count = 0
+  
+  while count <= array.length do
+    new_array = array[count**2]
+    count += 1
+   end
+   return new_array
 end
+
+# array = [1, 2, 3]
+# need new_array = [1, 4, 9]
